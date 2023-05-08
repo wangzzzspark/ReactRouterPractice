9130771ee506b94caf21bf591c0714dbd1572b0c
apple <apple@appledeMacBook-Air.local>
2023-05-08 17:57:14 +0800

    first commit to push the basic react-router

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+const Home = () => {
+   return (<>
+    <h3>
+        Home page
+    </h3>
+     
+   </>)
+    
+}
+export default Home
---
+++ src/pages/User.tsx
@@ -0,0 +1,11 @@
+ import React from "react"
+
+   const User = () => {
+    return <>
+        <h3>
+            User page
+        </h3>
+    </>
+}
+export default User
+  
